dfa90f9ca4fdf4a6c208de4200f74cd6c2c3ce97
y-mouatif <117135148+y-mouatif@users.noreply.github.com>
2025-03-01 23:55:41 -0500

    Update indoor-map.jsx

--- myApp/app/(tabs)/indoor-map.jsx
+++ myApp/app/(tabs)/indoor-map.jsx
@@ -1,32 +1,31 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import Mapbox from '@rnmapbox/maps';
 
 Mapbox.setAccessToken('pk.eyJ1IjoiN2FuaW5lIiwiYSI6ImNtN28yZ3V1ejA3Mnoya3B3OHFuZWJvZ2sifQ.6SOCiju5AqaC_cBBW7eOEw');
 
-const App = () => {
+const IndoorMap = () => {
   return (
-    <View style={styles.page}>
-      <View style={styles.container}>
-        <Mapbox.MapView style={styles.map} />
-      </View>
+    <View style={styles.container}>
+      <Text style={styles.title}>Map Title</Text>
+      <Mapbox.MapView style={styles.map} />
     </View>
   );
-}
+};
 
-export default App;
+export default IndoorMap;
 
 const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   container: {
-    height: 300,
-    width: 300,
+    flex: 1, // Fill the entire screen
+  },
+  title: {
+    fontSize: 24,
+    textAlign: 'center',
+    paddingVertical: 20,
+    backgroundColor: '#f0f0f0',
   },
   map: {
-    flex: 1
-  }
+    flex: 1, // Map takes the rest of the space
+  },
 });
