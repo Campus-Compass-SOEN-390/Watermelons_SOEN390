7e58147140815a0529c40501a3b042712e1b7e09
Hawraa Al-Adilee <hawraa.aladilee@gmail.com>
2025-03-29 02:05:24 -0400

    fix small bug, move styles

--- myApp/app/screens/SettingsPage.js
+++ myApp/app/screens/SettingsPage.js
@@ -1,21 +1,22 @@
-import React from "react";
-import { Switch, Text, View, StyleSheet, Image } from 'react-native';
-import { FeedbackProvider, useFeedback } from '../context/FeedbackContext';
+import React, { useState } from "react";
+import { Switch, Text, View, Image } from 'react-native';
+import { useFeedback } from '../context/FeedbackContext';
 import LayoutWrapper from "../components/LayoutWrapper";
 import HeaderButtons from "../components/HeaderButtons";
 import { Ionicons } from "@expo/vector-icons";
+import styles from '../styles/SettingsPageStyles';
 
 export default function Settings() {
   const {
     vibrationEnabled,
     soundEnabled,
     speechEnabled,
-    blackAndWhiteEnabled,
     toggleVibration,
     toggleSound,
     toggleSpeech,
-    toggleBlackAndWhite,
-  } = useFeedback(previousState => !previousState);
+  } = useFeedback();
+
+  const [blackMode, setBlackMode] = useState(false);
 
   return (
     <LayoutWrapper>
@@ -25,14 +26,15 @@ export default function Settings() {
         <Text style={styles.subtitle}>Feedback</Text>
 
         <View style={styles.settingRow}>
-          <Ionicons name="invert-mode-outline" size={24} color={blackAndWhiteEnabled ? "green" : "black"} />
+          <Ionicons name="invert-mode-outline" size={24} color='black' />
           <Text style={styles.settingLabel}>Black & White Mode</Text>
           <Switch
-            value={blackAndWhiteEnabled}
-            onValueChange={toggleBlackAndWhite}
+            value={blackMode}
+            onValueChange={() => setBlackMode(prevState => !prevState)}
             trackColor={{ false: '#b0b0b0', true: 'red' }}
             thumbColor={'grey'}
             style={styles.switch}
+            testID="black-mode-toggle"
           />
         </View>
 
@@ -48,6 +50,7 @@ export default function Settings() {
             trackColor={{ false: '#b0b0b0', true: 'red' }}
             thumbColor={'grey'}
             style={styles.switch}
+            testID="vibration-toggle"
           />
         </View>
 
@@ -60,6 +63,7 @@ export default function Settings() {
             trackColor={{ false: '#b0b0b0', true: 'red' }}
             thumbColor={'grey'}
             style={styles.switch}
+            testID="sound-toggle"
           />
         </View>
 
@@ -72,51 +76,10 @@ export default function Settings() {
             trackColor={{ false: '#b0b0b0', true: 'red' }}
             thumbColor={'grey'}
             style={styles.switch}
+            testID="speech-toggle"
           />
         </View>
       </View>
     </LayoutWrapper>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 20,
-    gap: 20
-  },
-  title: {
-    fontSize: 26,
-    fontWeight: 'bold',
-    marginBottom: 20,
-    color: '#333'
-  },
-  subtitle: {
-    color: "#666",
-    fontSize: 20,
-    marginBottom: 10
-  },
-  settingRow: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    backgroundColor: "#f8f8f8",
-    padding: 12,
-    borderRadius: 12,
-    marginVertical: 8,
-    elevation: 3,  // Add shadow effect for elevation
-  },
-  settingLabel: {
-    fontSize: 18,
-    fontWeight: '500',
-    color: "#333"
-  },
-  switch: {
-    transform: [{ scaleX: 1.5 }, { scaleY: 1.5 }],
-    right: 10,
-  },
-  vibration_image: {
-    width: 30,
-    height: 30,
-    marginRight: 12,
-  }
-});
