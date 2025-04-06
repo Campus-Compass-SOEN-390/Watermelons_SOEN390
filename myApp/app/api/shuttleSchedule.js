70e9194e35b65fa088a51e2b4e684114944bf303
Carmoucha <carmenderderian@gmail.com>
2025-04-06 17:38:49 -0400

    Improve test coverage for shuttleSchedule.js

--- myApp/app/api/shuttleSchedule.js
+++ myApp/app/api/shuttleSchedule.js
@@ -1,22 +1,6 @@
 import { getAllShuttleSchedules, getShuttleScheduleByDay } from "./shuttleScheduleData.js";
 import { getRealTimeShuttleData, extractShuttleInfo } from "./shuttleLiveData.js";
 
-// Enable this to simulate schedule for testing
-const TEST_MODE = false;
-
-/**
- * Return a mocked shuttle schedule for testing purposes.
- */
-const getMockSchedule = () => {
-  const now = new Date();
-  const currentHour = now.getHours();
-  const nextHour = currentHour + 1;
-
-  return {
-    SGW: [`${currentHour}:${now.getMinutes() + 2}`, `${currentHour}:${now.getMinutes() + 15}`],
-    LOY: [`${nextHour}:${now.getMinutes() + 5}`, `${nextHour}:${now.getMinutes() + 20}`],
-  };
-};
 
 // Fetch all shuttle schedules
 export const fetchAllShuttleSchedules = async () => getAllShuttleSchedules();
